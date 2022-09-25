168d3cff9b44624debe8b2ea200a7fa8bcd3bc64
Kirill <kirill.bnf@gmail.com>
2022-09-25 21:35:33 +0300

    add get api response

--- GIIS-lab2/GIIS-lab2/Program.cs
+++ GIIS-lab2/GIIS-lab2/Program.cs
@@ -1,6 +1,35 @@
+List<Contact> contacts = new List<Contact>()
+{
+    new Contact(){ Name = "Alex", Address="Kolotushkino" },
+    new Contact(){ Name = "Dranik", Address="Bombass" }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/api/contacts", () => contacts);
+app.MapGet("/api/contacts/{name}", (string name) =>
+{
+
+});
+app.MapGet("/api/contacts/{address}", (string address) =>
+{
+
+});
+app.MapPut("/api/contacts", (Contact contact) =>
+{
+
+});
+app.MapPost("/api/contacts", (Contact contact) =>
+{
+
+});
+app.MapDelete("/api/contacts/{name}/{address}", (string name, string address) =>
+{
+
+});
 
 app.Run();
