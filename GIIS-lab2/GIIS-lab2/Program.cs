e50b70b4b0d609789cacdc943726c61bce7cf89c
Kirill <kirill.bnf@gmail.com>
2022-09-25 22:18:27 +0300

    add all api contracts

--- GIIS-lab2/GIIS-lab2/Program.cs
+++ GIIS-lab2/GIIS-lab2/Program.cs
@@ -11,25 +11,50 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/api/contacts", () => contacts);
-app.MapGet("/api/contacts/{name}", (string name) =>
+app.MapGet("/api/contact/{name}/{address}", (string name, string address) =>
 {
+    Contact? contact = contacts.FirstOrDefault(c => c.Name.Equals(name) && c.Address.Equals(address));
 
+    if (contact == null)
+        return Results.NotFound(new { message = "Contact not found"});
+    
+    return Results.Json(contact);
 });
-app.MapGet("/api/contacts/{address}", (string address) =>
+app.MapPut("/api/contacts", (Contact contactData) =>
 {
+    Contact? contact = contacts.FirstOrDefault(c => c.Name.Equals(contactData.Name) && c.Address.Equals(contactData.Address));
+    if (contact == null)
+        return Results.NotFound(new { message = "Contact not found" });
 
+    contact.Name = contactData.Name;
+    contact.Address = contactData.Address;
+
+    return Results.Json(contact);
 });
-app.MapPut("/api/contacts", (Contact contact) =>
+app.MapPost("/api/contacts", (Contact contactData) =>
 {
+    Contact? contact = contacts.FirstOrDefault(c => c.Name.Equals(contactData.Name) && c.Address.Equals(contactData.Address));
 
+    if (contact == null)
+    {
+        contacts.Add(contactData);
+    }
+    else
+    {
+        return Results.Conflict(new { message = "Contact already exist" });
+    }
+    return Results.Json(contactData);
 });
-app.MapPost("/api/contacts", (Contact contact) =>
+app.MapDelete("/api/contact/{name}/{address}", (string name, string address) =>
 {
+    Contact? contact = contacts.FirstOrDefault(c => c.Name.Equals(name) && c.Address.Equals(address));
 
-});
-app.MapDelete("/api/contacts/{name}/{address}", (string name, string address) =>
-{
+    if (contact == null)
+        return Results.NotFound(new { message = "Contact not found" });
+    
+    contacts.Remove(contact);
 
+    return Results.Json(contact);
 });
 
 app.Run();
