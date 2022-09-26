54448df39b33ff91a5beee2fdc8e7b0e6265630d
Kirill <kirill.bnf@gmail.com>
2022-09-26 16:18:27 +0300

    add working front api

--- GIIS-lab2/GIIS-lab2/wwwroot/script.js
+++ GIIS-lab2/GIIS-lab2/wwwroot/script.js
@@ -14,9 +14,105 @@ async function getContacts() {
    }
 };
 
+async function getContact(id) {
+   const response = await fetch(`/api/contact/${id}`, {
+      method: "GET",
+      headers: { "Accept": "application/json" }
+   });
+   if (response.ok === true) {
+      const contact = await response.json();
+      document.getElementById("contactId").value = contact.id;
+      document.getElementById("ContactName").value = contact.name;
+      document.getElementById("ContactAddress").value = contact.address;
+   }
+   else {
+      const error = await response.json();
+      console.log(error.message);
+   }
+}
+
+async function createContact(contactName, contactAddress) {
+
+   const response = await fetch("api/contacts", {
+      method: "POST",
+      headers: { "Accept": "application/json", "Content-Type": "application/json" },
+      body: JSON.stringify({
+         id: guid(),
+         name: contactName,
+         address: contactAddress
+      })
+   });
+   if (response.ok === true) {
+      const contact = await response.json();
+      document.querySelector("tbody").append(row(contact));
+   }
+   else {
+      const error = await response.json();
+      console.log(error.message);
+   }
+}
+
+async function editContact(contactId, contactName, contactAddress) {
+   const response = await fetch("api/contacts", {
+      method: "PUT",
+      headers: { "Accept": "application/json", "Content-Type": "application/json" },
+      body: JSON.stringify({
+         id: contactId,
+         name: contactName,
+         address: contactAddress
+      })
+   });
+   if (response.ok === true) {
+      const contact = await response.json();
+      document.querySelector(`tr[data-rowid='${contact.id}']`).replaceWith(row(contact));
+   }
+   else {
+      const error = await response.json();
+      console.log(error.message);
+   }
+}
+
+async function deleteContact(id) {
+   const response = await fetch(`/api/contact/${id}`, {
+      method: "DELETE",
+      headers: { "Accept": "application/json" }
+   });
+   if (response.ok === true) {
+      const contact = await response.json();
+      document.querySelector(`tr[data-rowid='${contact.id}']`).remove();
+   }
+   else {
+      const error = await response.json();
+      console.log(error.message);
+   }
+}
+
+function reset() {
+   document.getElementById("contactId").value =
+      document.getElementById("ContactName").value =
+      document.getElementById("ContactAddress").value = "";
+}
+
+document.getElementById("resetBtn").addEventListener("click", () => reset());
+
+document.getElementById("saveBtn").addEventListener("click", async () => {
+   const id = document.getElementById("contactId").value;
+   const name = document.getElementById("ContactName").value;
+   const address = document.getElementById("ContactAddress").value;
+   console.log(id)
+   if (id !== "") {
+      await editContact(id, name, address);
+   }
+   else {
+      await createContact(name, address);
+   }
+   reset();
+});
+
 function row(contact) {
 
    const tr = document.createElement("tr");
+   tr.setAttribute("data-rowid", contact.id)
 
    const nameTd = document.createElement("td");
    nameTd.append(contact.name);
@@ -26,21 +122,28 @@ function row(contact) {
    addressTd.append(contact.address);
    tr.append(addressTd);
 
-   //   const linksTd = document.createElement("td");
+   const linksTd = document.createElement("td");
 
-   //   const editLink = document.createElement("button");
-   //   editLink.append("Modify");
-   //   editLink.addEventListener("click", async () => await getUser(contact.id));
-   //   linksTd.append(editLink);
-   //
-   //   const removeLink = document.createElement("button");
-   //   removeLink.append("Удалить");
-   //   removeLink.addEventListener("click", async () => await deleteUser(contact.id));
+   const editLink = document.createElement("button");
+   editLink.append("Modify");
+   console.log(`${contact.id} + ${contact.name}`)
+   editLink.addEventListener("click", async () => await getContact(contact.id));
+   linksTd.append(editLink);
 
-   //   linksTd.append(removeLink);
-   //   tr.appendChild(linksTd);
+   const removeLink = document.createElement("button");
+   removeLink.append("Remove");
+   removeLink.addEventListener("click", async () => await deleteContact(contact.id));
+   linksTd.append(removeLink);
+
+   tr.appendChild(linksTd);
 
    return tr;
+}
+
+function guid() {
+   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+   );
 }
 
 getContacts();
