e74f2c8e4321decae6ccbc7aba64ce731cc70274
Kirill <kirill.bnf@gmail.com>
2022-09-26 21:47:04 +0300

    Add ef with MsSqlDB

--- GIIS-lab2/GIIS-lab2/Entities/Contact.cs
+++ GIIS-lab2/GIIS-lab2/Entities/Contact.cs
@@ -1,7 +1,15 @@
-﻿//[Index(nameof(Name), (nameof(Address)))]
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+[Index(nameof(Name), (nameof(Address)))]
 public class Contact
 {
+    [Required]
     public Guid Id { get; set; }
+    [Required]
+    [MinLength(3)]
     public string Name { get; set; }
+    [Required]
+    [MinLength(3)]
     public string Address { get; set; }
 }
---
+++ GIIS-lab2/GIIS-lab2/Repository/RepositoryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GIIS_lab2.Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; } = null!;
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Contact>().HasData(
+                     new Contact() { Id = Guid.NewGuid(), Name = "Alex", Address = "Kolotushkino" },
+                     new Contact() { Id = Guid.NewGuid(), Name = "Dranik", Address = "Bombass" }
+                );
+        }
+
+    }
+}
