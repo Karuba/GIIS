36f374c22a40a922043906f478a375623a16d9a0
Kirill <kirill.bnf@gmail.com>
2022-09-27 21:16:44 +0300

    backend: present contacts file with create file in the server

--- GIIS-lab2/GIIS-lab2/Program.cs
+++ GIIS-lab2/GIIS-lab2/Program.cs
@@ -8,7 +8,22 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapGet("/api/contacts", (RepositoryContext db) => db.Contacts);
+
+app.MapGet("/api/contacts", async (RepositoryContext db) => await db.Contacts.ToListAsync());
+app.MapGet("/api/contacts/file", async (RepositoryContext db) =>
+{
+    string path = @"C:\WorkPlace\University-4-7\labs\\GIIS\GIIS-lab2\GIIS-lab2\wwwroot\Files\Contacts.txt";
+
+    File.Delete(path);
+    StreamWriter writer = new StreamWriter(path, true);
+
+    var contacts = await db.Contacts.ToListAsync();
+    contacts.ForEach(c => writer.WriteLine($"Name: {c.Name}, Address: {c.Address};"));
+    writer.Close();
+
+
+    return Results.File(File.ReadAllBytes(path), "text/plain");
+});
 app.MapGet("/api/contact/{id}", async (Guid id, RepositoryContext db) =>
 {
     Contact? contact = await db.Contacts.FirstOrDefaultAsync(c => c.Id.Equals(id));
